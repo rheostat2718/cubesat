c529a1c55f9f839db584babc4460c6dfda2bd5b8
rheostat2718 <caleb.cain1984@gmail.com>
2013-11-08 18:58:05 -0600

    Create usbUART.c

---
+++ usbUART.c
@@ -0,0 +1,61 @@
+/*
+ * usbUART.c
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: cmcain
+ */
+
+
+#define PART_TM4C123GH6PM
+
+#include "driverlib/pin_map.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/uart.h"
+#include <string.h>
+#include "usbUART.h"
+
+
+
+void myusbInit(void)
+{
+	SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 9600,
+	(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+}
+
+void echoLoop()
+{
+	UARTCharPut(UART0_BASE, 'E');
+	UARTCharPut(UART0_BASE, 'n');
+	UARTCharPut(UART0_BASE, 't');
+	UARTCharPut(UART0_BASE, 'e');
+	UARTCharPut(UART0_BASE, 'r');
+	UARTCharPut(UART0_BASE, ' ');
+	UARTCharPut(UART0_BASE, 'T');
+	UARTCharPut(UART0_BASE, 'e');
+	UARTCharPut(UART0_BASE, 'x');
+	UARTCharPut(UART0_BASE, 't');
+	UARTCharPut(UART0_BASE, ':');
+	UARTCharPut(UART0_BASE, ' ');
+	while (1)
+	{
+	if (UARTCharsAvail(UART0_BASE)) UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
+	}
+
+}
