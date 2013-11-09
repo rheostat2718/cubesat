f060a3bc13ce25f7b79210f74fa468eef2436bd2
rheostat2718 <caleb.cain1984@gmail.com>
2013-11-08 18:58:16 -0600

    Create usbUART.h

---
+++ usbUART.h
@@ -0,0 +1,18 @@
+/*
+ * usbUART.h
+ *
+ *  Created on: Nov 8, 2013
+ *      Author: cmcain
+ */
+
+#ifndef USBUART_H_
+#define USBUART_H_
+
+void myusbInit(void);
+
+void echoLoop(void);
+
+
+
+
+#endif /* USBUART_H_ */
